695e5e3a71e4e18186c7aaae99f6c366b8aa4708
ThomB96 <thom_berkvens@hotmail.com>
2025-12-31 15:14:26 +0100

    Updates

--- client/src/components/examples/ContactForm.tsx
+++
@@ -1,9 +0,0 @@
-import ContactForm from '../ContactForm';
-
-export default function ContactFormExample() {
-  return (
-    <div className="max-w-2xl p-6">
-      <ContactForm />
-    </div>
-  );
-}
